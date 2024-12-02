e04230af10f0e0773b17153fe1275896ee479d5a
Grace Wilson <gwilson011@gmail.com>
2024-12-01 17:13:41 -0700

    project card animation update

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -5,60 +5,59 @@ import { useState } from "react";
 
 interface Project {
     title: string;
+    year: number;
     desc: string;
     image: string;
     href: { demo: string; repo: string };
 }
 
-interface Socialbarproject {
+interface ProjectCardProject {
     project: Project;
 }
 
-const Socialbar = ({ project }: Socialbarproject) => {
+const ProjectCard = ({ project }: ProjectCardProject) => {
     const [hover, setHover] = useState<boolean>(false);
     return (
         <div
-            className="flex flex-row gap-2 border-default text-black p-4 justify-between hover:text-white hover:bg-black"
-            onMouseEnter={() => setHover && setHover(true)}
-            onMouseLeave={() => setHover && setHover(false)}
+            className="h-[74vh] space-y-4 border-default text-black p-4 justify-between bg-white" //hover:text-white hover:bg-black"
+            // onMouseEnter={() => setHover && setHover(true)}
+            // onMouseLeave={() => setHover && setHover(false)}
         >
-            <div className="flex flex-col">
-                <span className="font-tango text-3xl">{project.title}</span>
-                {hover && (
+            <div className="flex flex-col justify-start gap-6 h-full p-5">
+                <div className="flex flex-row justify-between content-end">
+                    <span className="font-tango text-3xl">{project.title}</span>
+                    <span className="font-pixel text-xs">{project.year}</span>
+                </div>
+
+                <div className="flex flex-col gap-6">
                     <div className="font-louis flex-1 break-words overflow-y-auto max-w-80">
                         {project.desc}
                     </div>
-                )}
-            </div>
-            {hover && (
-                <div className="flex flex-row gap-2">
-                    <Image
-                        alt="home"
-                        src={project.image}
-                        width={200}
-                        height={80}
-                        className="rounded w-full overflow-hidden"
-                    />
-                    <div className="flex flex-col gap-2">
-                        <Link
-                            href={project.href.demo}
-                            className="flex p-1 px-2 bg-white border-default text-black font-tango text-md hover:text-white hover:bg-black"
-                            target="_blank"
-                        >
-                            DEMO
-                        </Link>
-                        <Link
-                            href={project.href.repo}
-                            className="flex p-1 px-2 bg-white border-default text-black font-tango text-md hover:text-white hover:bg-black"
-                            target="_blank"
-                        >
-                            REPO
-                        </Link>
-                    </div>
+                    <Link
+                        href={project.href.demo}
+                        className="flex text-center justify-center p-2 px-2 bg-white border-default text-black font-tango text-md hover:text-white hover:bg-black"
+                        target="_blank"
+                    >
+                        DEMO
+                    </Link>
+                    <Link
+                        href={project.href.repo}
+                        className="flex text-center justify-center p-2 px-2 bg-white border-default text-black font-tango text-md hover:text-white hover:bg-black"
+                        target="_blank"
+                    >
+                        REPO
+                    </Link>
                 </div>
-            )}
+                <Image
+                    alt="home"
+                    src={project.image}
+                    width={200}
+                    height={80}
+                    className="rounded w-full overflow-hidden"
+                />
+            </div>
         </div>
     );
 };
 
-export default Socialbar;
+export default ProjectCard;
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,13 +1,52 @@
+"use client";
 import Navbar from "../components/Navbar";
 import Socialbar from "../components/Socialbar";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import ProjectCard from "../components/ProjectCard";
+import {
+    useScroll,
+    useMotionValueEvent,
+    useTransform,
+    motion,
+} from "framer-motion";
 
 import projects from "./projects.json";
 
 export default function Projects() {
+    const targetRef = useRef<HTMLDivElement>(null);
+    const { scrollY } = useScroll({
+        target: targetRef,
+        offset: ["start start", "end end"],
+    });
+
+    useMotionValueEvent(scrollY, "change", () => {
+        console.log(scrollY.get());
+    });
+
+    const cardTimeline = projects.map((_, i) => {
+        const start = 500 + i * 500;
+        const end = 500 + (i + 1) * 500;
+        return [start, end];
+    });
+
+    const timeline = [[0, 500], ...cardTimeline];
+
+    const animation = timeline.map((data) => ({
+        scale: useTransform(scrollY, data, [1, 0.8]),
+        opacity: useTransform(scrollY, data, [1, 0]),
+    }));
+
+    useEffect(() => {
+        // Get the div element
+        let divElement = document.getElementById("card");
+        // Scroll to the bottom of the div
+        if (divElement != null) {
+            divElement.scrollTop = divElement.scrollHeight;
+        }
+    }, []);
+
     return (
-        <div className="flex flex-col w-full h-screen p-4 gap-4 md:gap-6">
+        <div className="flex flex-col w-full h-[90vh] p-4 gap-4 md:gap-6">
             <Socialbar />
             <Navbar home={true} />
             <div className="flex flex-col gap-32 md:flex-row h-full md:mr-24 md:ml-24 mb-10">
@@ -16,10 +55,29 @@ export default function Projects() {
                         PROJECTS
                     </span>
                 </div>
-                <div className="flex flex-col w-full h-full gap-3">
-                    {Object.entries(projects).map(([key, project]) => (
-                        <ProjectCard key={key} project={project} />
-                    ))}
+                <div
+                    ref={targetRef}
+                    className="space-y-20 h-full relative overflow-auto"
+                    id="card"
+                >
+                    <div className="flex flex-col gap-4">
+                        {Object.entries(projects).map(([key, project], i) => (
+                            <motion.div
+                                className={`h-full sticky text-black`}
+                                style={{
+                                    top: `${i * 25}px`,
+                                    // scale: animation[i + 1].scale,
+                                    // opacity: animation[i + 1].opacity,
+                                }}
+                            >
+                                <ProjectCard
+                                    key={key}
+                                    project={{ ...project }}
+                                />
+                            </motion.div>
+                        ))}
+                        <div className="flex h-full"></div>
+                    </div>
                 </div>
             </div>
         </div>
