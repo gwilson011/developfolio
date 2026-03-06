a80b5c764e8f53f277c3d46fe8bc42f2a17296d1
Grace Wilson <gwilson011@gmail.com>
2026-03-06 00:04:53 -0800

    simplified grocery card

---
+++ src/app/mine/cards/GroceryCard.tsx
@@ -0,0 +1,21 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+
+const GroceryCard = () => {
+    return (
+        <Link href="/groceries">
+            <div className="border-default rounded text-black p-1 pr-2 cursor-pointer">
+                <Image
+                    src="/mine/grocery.png"
+                    alt="Grocery"
+                    width={100}
+                    height={50}
+                    className="rounded"
+                />
+            </div>
+        </Link>
+    );
+};
+
+export default GroceryCard;
